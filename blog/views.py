1fa1aa1eb00a0371375da99a3fb7834bcdfd345b
imcreator <imcreator3@gmail.com>
2017-07-01 14:34:57 +0900

    minor change

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,6 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
