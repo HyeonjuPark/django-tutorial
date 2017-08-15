aaa985ff23f10c2bb5b94ad9db95bfd7c5cf8e25
imcreator <imcreator3@gmail.com>
2017-08-15 13:39:07 +0900

    모델 스크래퍼로 재구성
    PostSite: 스크랩할 URL 정보를 가지고 있음
    Post:  스크랩한 정보를 저장함
    PostItem: 스크랩한 정보를 담을 모델을 지정함

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, PostSite
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'author':
+            kwargs['initial'] = request.user.id
+        return super(PostAdmin, self).formfield_for_foreignkey(
+            db_field, request, **kwargs
+        )
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(PostSite)
--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,52 @@
 from django.db import models
 from django.utils import timezone
+from dynamic_scraper.models import Scraper, SchedulerRuntime
+from scrapy_djangoitem import DjangoItem
+from django.dispatch import receiver
+from django.db.models.signals import pre_delete
+
+@receiver(pre_delete)
+def pre_delete_handler(sender, instance, using, **kwargs):
+    if isinstance(instance, Post):
+        if instance.checker_runtime:
+            instance.checker_runtime.delete()
+
+pre_delete.connect(pre_delete_handler)
+
+class PostSite(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField()
+    scraper = models.ForeignKey(Scraper, blank=True, null=True, on_delete=models.SET_NULL)
+    scraper_runtime = models.ForeignKey(SchedulerRuntime, blank=True, null=True, on_delete=models.SET_NULL)
+
+    def __unicode__(self):
+        return self.name
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User',
+                               blank=True,
+                               null=True,)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    image = models.CharField(max_length=200)
+    post_site = models.ForeignKey(PostSite)
+    url = models.URLField()
+    checker_runtime = models.ForeignKey(SchedulerRuntime, blank=True, null=True, on_delete=models.SET_NULL)
+    foo = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True
+    )
+
+    def setfoo(self, x):
+        self.foo = json.dumps(x)
+
+    def getfoo(self):
+        return json.loads(self.foo)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -16,3 +54,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class PostItem(DjangoItem):
+    django_model = Post
